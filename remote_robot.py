09c6fe1cd68fea93e876b7e87ef2ebe399d460ea
e1234573123 <e1234573123@penguin>
2020-04-05 18:34:05 +0800

    Add get changed file between two commits

--- remote_robot.py
+++ remote_robot.py
@@ -1,6 +1,8 @@
 import click
 import paramiko
 import re
+import shlex
+import subprocess
 from env import *
 
 
@@ -22,21 +24,70 @@ def ssh_copy(method, localpath, remotepath):
     t.close()
 
 
+def shell_caller(cmd):
+    resp = subprocess.Popen(cmd,
+                            shell=True,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            encoding="utf-8")
+    out, err = resp.communicate()
+    if resp.poll() == 0:
+        return out
+    else:
+        return err
+
+
+def git_local_file_diff(project, first, second):
+    cmd = "git -C {}/{project} diff --name-only {first} {second}".format(
+        LOCAL_PATH,**locals())
+    resp = shell_caller(cmd)
+    return resp.split("\n")
+
+
+def git_current_commit(project, branch, remote=False):
+    if remote:
+        ssh = ssh_connect()
+        pre_cmd = "{REMOTE_BASE_PATH}/{project} git pull".format(locals())
+        ssh.close()
+    else:
+        shell_caller(
+            "git -C {}/{project} pull && git -C {}/{project} checkout {branch}"
+            .format(LOCAL_PATH, LOCAL_PATH, **locals()))
+        out = shell_caller("git -C {}/{project} rev-parse HEAD".format(
+            LOCAL_PATH, **locals()))
+    return "".join(out.split("\n"))
+
+
 @click.group()
 def cli():
     pass
 
 
-@click.command()
-@click.option("robot", help="Remote the robot command by ssh")
+@click.command("test", help="test option")
+@click.option("-p", "--project", help="The folder need to run")
+@click.option("-b", "--branch", help="The branch need to run")
+@click.option("-r",
+              "--remote",
+              default=None,
+              required=False,
+              help="The remote need to run")
+def test(project, branch, remote):
+    first = git_current_commit(project, branch, remote)
+    git_local_file_diff(project,first,"b6d77b68b030bd4d38947f3cc7b981f6037c6f1f")
+    print(first)
+
+
+@click.command("robot", help="Remote the robot command by ssh")
 @click.option("-c", "--command", help="The command need to run")
 def remote_robot(command):
     ssh = ssh_connect()
     stdin, stdout, stderr = ssh.exec_command(command)
-    project = re.match(r'(-I) (?P<project>dqa-\w+)').group("project")
+    project = re.match(r'(-I) (?P<project>dqa-\w+)', stdout).group("project")
     ssh_copy("get", "", "")
 
 
+cli.add_command(test)
+cli.add_command(remote_robot)
 
 if __name__ == "__main__":
     USER = input("User name: ") if USER == "" else USER
@@ -51,3 +102,8 @@ if __name__ == "__main__":
 # python dqa-script/exo-robot-runner run -I dqa-exosense -i api \
 # -O --suite=request_removal \
 # --docker_skip
+
+# p=subprocess.Popen("dir", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# (stdoutput,erroutput) = p.communicate()
+
+# &&
