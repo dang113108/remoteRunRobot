2e80aab5dd629b6c2247b9e41a4963c5d42262b9
Markchang <markchang@exosite.com>
2020-04-09 16:36:13 +0800

    Update run robot process

--- remote_robot.py
+++ remote_robot.py
@@ -18,20 +18,24 @@ def ssh_connect():
     ssh.connect(IP_HOST, 22, USER, PWD)
     return ssh
 
+
 def get_remote_report_folder(project):
-    ssh= ssh_connect()
-    remotepath=REMOTE_BASE_PATH+"/report/{project}/general_report".format(**locals())
+    ssh = ssh_connect()
+    remotepath = REMOTE_BASE_PATH + \
+        "/report/{project}/general_report".format(**locals())
     with SCPClient(ssh.get_transport()) as scp:
-        scp.get(remotepath, LOCAL_PATH+"/remote_report", recursive=True)
+        scp.get(remotepath, LOCAL_PATH +
+                "/remote_report/{project}/general_report".format(**locals()), recursive=True)
 
 
 def sync_remote_file(localpath, remotepath):
-    ssh=ssh_connect()
+    ssh = ssh_connect()
     with SCPClient(ssh.get_transport()) as scp:
         for local, remote in zip(localpath, remotepath):
             logging.info("Sync {} to {}".format(local, remote))
             scp.put(local, remote)
         logging.info("Sync complete")
+
 
 def git_status(path, status):
     if not path:
@@ -39,8 +43,9 @@ def git_status(path, status):
     r = shell_caller("git -C {} status -s".format(path))
     lines = []
     for line in r.splitlines():
-        if re.match(r'(%s) (?P<file>\w+)'%(status),line):
-            lines.append(re.match(r'(%s) (?P<file>.+)'%(status),line).group("file"))
+        if re.match(r'(%s) (?P<file>\w+)' % (status), line):
+            lines.append(re.match(r'(%s) (?P<file>.+)' %
+                                  (status), line).group("file"))
     return lines
 
 
@@ -49,7 +54,6 @@ def ssh_copy(method, localpath, remotepath):
         t = paramiko.Transport((IP_HOST, 22))
         t.connect(username=USER, password=PWD)
         sftp = paramiko.SFTPClient.from_transport(t)
-        # if stat.S_ISDIR(remotepath):
         if len(localpath) != len(remotepath):
             raise AssertionError("file path is not the same")
         for local, remote in zip(localpath, remotepath):
@@ -72,15 +76,23 @@ def shell_caller(cmd):
     return out if resp.poll() == 0 else err
 
 
-def ssh_shell_caller(cmd):
+def ssh_shell_caller(cmd, robot=False):
     try:
         ssh = ssh_connect()
         if re.match(r'sudo', cmd):
-            stdin, stdout, stderr = ssh.exec_command(cmd,get_pty=True)
+            logging.info("-----Run sudo------")
+            logging.info(cmd)
+            if robot:
+                stdin, stdout, stderr = ssh.exec_command(
+                    "cd %s && %s" % (REMOTE_BASE_PATH, cmd), get_pty=True)
+            else:
+                stdin, stdout, stderr = ssh.exec_command(cmd, get_pty=True)
+            stdin.write('%s\n' % PWD)
+            stdin.flush()
         else:
             stdin, stdout, stderr = ssh.exec_command(cmd)
-        out=stdout.read().decode()
-        err=stderr.read().decode()
+        out = stdout.read().decode()
+        err = stderr.read().decode()
         logging.info(out if stdout.channel.recv_exit_status(
         ) == 0 else err)
         return out if stdout.channel.recv_exit_status() == 0 else err
@@ -92,10 +104,11 @@ def git_local_file_diff(project, first, second):
     cmd = "git -C {}/{project} diff --name-only {first} {second}".format(
         LOCAL_PATH, **locals())
     resp = shell_caller(cmd)
-    diff_file=list(filter(None, resp.split("\n")))
+    diff_file = list(filter(None, resp.split("\n")))
     # use class
-    status_string=[" M","A ","\?\?"," R"]
-    status_list = list(filter(None,[git_status("{}/{}".format(LOCAL_PATH, project),status) for status in status_string]))
+    status_string = [" M", "A ", "\?\?", " R"]
+    status_list = list(filter(None, [git_status(
+        "{}/{}".format(LOCAL_PATH, project), status) for status in status_string]))
     # list(map(git_status("{}/{project}".format(LOCAL_PATH, **locals())," M")
     [diff_file.extend(x) for x in status_list]
     # map(lambda x: diff_file.extend(x),status_list)
@@ -108,6 +121,7 @@ def git_local_file_diff(project, first, second):
     logging.info(diff_file)
     return diff_file
     # -------------------------
+
 
 def git_current_commit(project, branch, remote=False):
     if remote:
@@ -123,11 +137,12 @@ def git_current_commit(project, branch, remote=False):
         out = shell_caller("git -C {}/{project} rev-parse HEAD".format(
             LOCAL_PATH, **locals()))
         commit = "".join(out.split("\n"))
-    logging.info("Remote Commit: "+commit if remote else "Local Commit: "+commit)
+    logging.info("Remote Commit: " +
+                 commit if remote else "Local Commit: "+commit)
     return commit
 
 
-def git_clean_and_back_to_branch(project,delete_branch, branch="master", remote=False):
+def git_clean_and_back_to_branch(project, delete_branch, branch="master", remote=False):
     path = REMOTE_BASE_PATH if remote else LOCAL_PATH
     ssh_shell_caller("git -C {}/{project} checkout -- .".format(
         path, **locals()))
@@ -144,13 +159,12 @@ def cli():
     pass
 
 
-@click.command("get_file", help="test option")
-@click.option("-p", "--project", help="The folder need to run")
-@click.option("-b", "--branch", help="The branch need to run")
-def get_changed_file(project, branch):
-    first = git_current_commit(project, branch)
-    second = git_current_commit(project, branch, remote=True)
-    resp = git_local_file_diff(project, first, second)
+@click.command("test", help="test option")
+# @click.option("-p", "--project", help="The folder need to run")
+# @click.option("-b", "--branch", help="The branch need to run")
+def test():
+    resp = ssh_shell_caller(
+        'sudo python dqa-script/exo-robot-runner run -I dqa-exosense -i web -O -t="Add Threshold rule should be succeed when click save" -b chrome -r0')
     print(resp)
 
 
@@ -159,31 +173,32 @@ def get_changed_file(project, branch):
 @click.option("-b", "--branch", help="The branch need to run")
 def remote_robot(command, branch):
     try:
-        shell_caller("mkdir remote_report")
         project = re.search(r'(-I) (?P<project>dqa-\w+)',
                             command).group("project")
+        shell_caller(
+            "mkdir -p remote_report/{}/general_report".format(project))
         first = git_current_commit(project, branch)
         second = git_current_commit(project, branch, remote=True)
         logging.info("local commit: "+first+"remote commit: "+second)
         resp = git_local_file_diff(project, first, second)
-        local_files= list(map(lambda file: LOCAL_PATH+"/"+str(project)+"/"+file, resp))
-        remote_files= list(map(lambda file: REMOTE_BASE_PATH+"/"+str(project)+"/"+file, resp))
+        local_files = list(map(lambda file: LOCAL_PATH +
+                               "/"+str(project)+"/"+file, resp))
+        remote_files = list(
+            map(lambda file: REMOTE_BASE_PATH+"/"+str(project)+"/"+file, resp))
         logging.info(local_files)
         logging.info(remote_files)
-        sync_remote_file(local_files,remote_files)
-        ssh_shell_caller(command)
-        get_remote_report_folder(project)        
+        sync_remote_file(local_files, remote_files)
+        ssh_shell_caller(command, True)
+        get_remote_report_folder(project)
+        logging.info("----log file----")
+        logging.info(
+            "{}/remote_report/{}/general_report/log.html".format(LOCAL_PATH, project))
 
     finally:
-        # git_clean_and_back_to_branch(branch)
         git_clean_and_back_to_branch(project, branch, remote=True)
-    # ssh = ssh_connect()
-    # stdin, stdout, stderr = ssh.exec_command(command)
-    # project = re.match(r'(-I) (?P<project>dqa-\w+)', stdout).group("project")
-    # ssh_copy("get", "", "")
 
 
-cli.add_command(get_changed_file)
+cli.add_command(test)
 cli.add_command(remote_robot)
 
 if __name__ == "__main__":
@@ -208,7 +223,7 @@ if __name__ == "__main__":
 #         dir_path = dir_path[0:-1]
 #     files = sftp.listdir_attr(dir_path)
 #     for x in files:
-#         # find subdir if there is 
+#         # find subdir if there is
 #         filename = dir_path + '/' + x.filename
 #         if stat.S_ISDIR(x.st_mode):
 #             all_files.extend(get_remote_folder_files(sftp, filename))
