537935800266ec72abb089b460056cbf7250c783
Markchang <markchang@exosite.com>
2020-05-31 18:30:11 +0800

    Update the log format and status judgement

--- remote_robot.py
+++ remote_robot.py
@@ -1,11 +1,8 @@
 import click
-import git_status
 import logging
 import os
 import paramiko
 import re
-import shlex
-import stat
 import subprocess
 from env import *
 from scp import SCPClient
@@ -20,21 +17,28 @@ def ssh_connect():
 
 
 def get_remote_report_folder(project):
-    ssh = ssh_connect()
-    remotepath = REMOTE_BASE_PATH + \
-        "/report/{project}/general_report".format(**locals())
-    with SCPClient(ssh.get_transport()) as scp:
-        scp.get(remotepath, LOCAL_PATH +
-                "/remote_report/{project}/general_report".format(**locals()), recursive=True)
-
+    try:
+        ssh = ssh_connect()
+        remotepath = REMOTE_BASE_PATH + \
+            "/report/{project}/general_report".format(**locals())
+        # socket_timeout=120
+        with SCPClient(ssh.get_transport(), socket_timeout=30) as scp:
+            scp.get(remotepath, LOCAL_PATH +
+                    "/remote_report/{project}/".format(**locals()), recursive=True)
+            logging.info("----log file----")
+            logging.info(LOCAL_PATH +
+                        "/remote_report/{project}/general_report/log.html".format(**locals()))
+        ssh_shell_caller(f"sudo rm -rf {remotepath[:-14]}")
+    finally:
+        ssh.close()
 
 def sync_remote_file(localpath, remotepath):
     ssh = ssh_connect()
     with SCPClient(ssh.get_transport()) as scp:
         for local, remote in zip(localpath, remotepath):
-            logging.info("Sync {} to {}".format(local, remote))
+            logging.info("---Sync File---\n {} to {}".format(local, remote))
             scp.put(local, remote)
-        logging.info("Sync complete")
+        logging.info("---Sync complete---")
 
 
 def git_status(path, status):
@@ -49,20 +53,15 @@ def git_status(path, status):
     return lines
 
 
-def ssh_copy(method, localpath, remotepath):
-    try:
-        t = paramiko.Transport((IP_HOST, 22))
-        t.connect(username=USER, password=PWD)
-        sftp = paramiko.SFTPClient.from_transport(t)
-        if len(localpath) != len(remotepath):
-            raise AssertionError("file path is not the same")
-        for local, remote in zip(localpath, remotepath):
-            if method == "get":
-                sftp.get(remote, local)
-            elif method == "put":
-                sftp.put(local, remote)
-    finally:
-        t.close()
+def scp_file(method, local, remote):
+    ssh = ssh_connect()
+    with SCPClient(ssh.get_transport()) as scp:
+        logging.info("---Sync File---\n {} to {}".format(local, remote))
+        if method=="put":
+            scp.put(local, remote)
+        elif method=="get":
+            scp.get(remote,local)
+        logging.info("---Sync complete---")
 
 
 def shell_caller(cmd):
@@ -106,17 +105,10 @@ def git_local_file_diff(project, first, second):
     resp = shell_caller(cmd)
     diff_file = list(filter(None, resp.split("\n")))
     # use class
-    status_string = [" M", "A ", "\?\?", " R"]
+    status_string = ["M ", "A ", "\?\?", " R"," M", "MM"]
     status_list = list(filter(None, [git_status(
         "{}/{}".format(LOCAL_PATH, project), status) for status in status_string]))
-    # list(map(git_status("{}/{project}".format(LOCAL_PATH, **locals())," M")
     [diff_file.extend(x) for x in status_list]
-    # map(lambda x: diff_file.extend(x),status_list)
-
-    # diff_file.extend(status.M)
-    # diff_file.extend(status.untracked) -->git status -s "??" not " ??"
-    # diff_file.extend(status.A)
-    # diff_file.extend(status.R)
     logging.info("---------* Diff Files: *---------")
     logging.info(diff_file)
     return diff_file
@@ -160,12 +152,11 @@ def cli():
 
 
 @click.command("test", help="test option")
-# @click.option("-p", "--project", help="The folder need to run")
-# @click.option("-b", "--branch", help="The branch need to run")
-def test():
-    resp = ssh_shell_caller(
-        'sudo python dqa-script/exo-robot-runner run -I dqa-exosense -i web -O -t="Add Threshold rule should be succeed when click save" -b chrome -r0')
-    print(resp)
+@click.option("-r", "--remote", help="The remote need to run")
+@click.option("-l", "--local", help="The local need to run")
+@click.option("-m", "--method", help="The method need to run")
+def test(remote, local, method):
+    scp_file(method, local, remote)
 
 
 @click.command("robot", help="Remote the robot command by ssh")
@@ -176,7 +167,7 @@ def remote_robot(command, branch):
         project = re.search(r'(-I) (?P<project>dqa-\w+)',
                             command).group("project")
         shell_caller(
-            "mkdir -p remote_report/{}/general_report".format(project))
+            "mkdir -p remote_report/{}/".format(project))
         first = git_current_commit(project, branch)
         second = git_current_commit(project, branch, remote=True)
         logging.info("local commit: "+first+"remote commit: "+second)
@@ -190,9 +181,6 @@ def remote_robot(command, branch):
         sync_remote_file(local_files, remote_files)
         ssh_shell_caller(command, True)
         get_remote_report_folder(project)
-        logging.info("----log file----")
-        logging.info(
-            "{}/remote_report/{}/general_report/log.html".format(LOCAL_PATH, project))
 
     finally:
         git_clean_and_back_to_branch(project, branch, remote=True)
